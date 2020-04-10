6fced15b757bca0dc9bacb9099392a21d77c29b5
japneetsingh <japneet2@illinois.edu>
2020-04-10 01:28:20 -0700

    List Comprehension with If-Else Conditional

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,11 +1,5 @@
-temps = [221, 234, 340, 230]
+temps = [221, 234, 340, -9999, 230]
 
-new_temps = []
-for temp in temps:
-    new_temps.append(temp / 10)
+new_temps = [temp / 10 if temp != -9999 else 0 for temp in temps]
 
 print(new_temps)
-
-
-even_newer_temps = [temp / 10 for temp in temps]
-print(even_newer_temps)
