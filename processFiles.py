d5a183528166303cb91549150cc13ed99053533d
japneetsingh <japneet2@illinois.edu>
2020-05-19 21:14:54 -0700

    appending text to an existing file

--- processFiles.py
+++ processFiles.py
@@ -4,7 +4,21 @@ with open("original.txt") as myfile:
 print(content)
 
 
-with open("./files/veegetables.txt", "w") as myfile:
+with open("./files/vegetables.txt", "w") as myfile:
     myfile.write("Tomato\nCucumber\nOnion\n")
     myfile.write("Garlic")
 
+
+'''    this will return error because vegetables.txt already exists
+
+with open("./files/vegetables.txt", "x") as myfile:
+    myfile.write("Okra")
+'''
+
+
+with open("./files/vegetables.txt", "a+") as myfile:
+    myfile.write("\nSpinach")
+    myfile.seek(0)
+    newContent = myfile.read()
+
+print(newContent)
